dcffc28a90c8b8e025d26e623f5038d5f7d1da7a
frankyhukom <frankyhukom@gmail.com>
2025-08-19 10:01:33 +0700

    Add model classes for Category, Item, Order, OrderItem, Role, and User with relationships and fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,25 +3,41 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'fullname',
+        'email',
+        'phone',
+        'role_id',
+        'created_at',
+        'updated_at',
+
+
     ];
+
+    protected $dates = ['deleted_at'];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.
