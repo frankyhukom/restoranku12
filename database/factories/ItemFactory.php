6cc64ce0e352cb29a53be7c8640b066517e14bb8
frankyhukom <frankyhukom@gmail.com>
2025-08-20 21:31:38 +0700

    Update itemFactory to use specific image URLs and modify DatabaseSedder to call itemSeede

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -22,7 +22,12 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2), // Assuming categories with IDs 1 and 2 exist
             'price' => $this->faker->randomFloat(2, 1000, 100000),
             'description' => $this->faker->text(),
-            'image' => $this->faker->imageUrl(),
+            //'image' => $this->faker->imageUrl(),
+            'image' => fake()->randomElement([
+                'https://images.unsplash.com/photo-1652752731860-ef0cf518f13a',
+                'https://images.unsplash.com/photo-1638866281450-3933540af86a',
+                'https://plus.unsplash.com/premium_photo-1674062989120-4ccc0eb35be0',
+            ]),
             'is_active' => $this->faker->boolean(),
         ];
     }
